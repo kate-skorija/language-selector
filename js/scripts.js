bcac96ec9bedb5170f24d99783a56bdc6700cc6a
Kate Skorija <kateskorija@gmail.com>
2020-06-05 12:42:19 -0500

    change radio button values back to descriptive strings, add reset button

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const workplace = parseInt($("input:radio[name=workplace]:checked").val());
-    const codeFun = parseInt($("input:radio[name=codeFun]:checked").val());
-    const color = parseInt($("input:radio[name=color]:checked").val());
-    const trait = parseInt($("input:radio[name=trait]:checked").val());
-    const location = parseInt($("input:radio[name=location]:checked").val());
+    const workplace = $("input:radio[name=workplace]:checked").val();
+    const codeFun = $("input:radio[name=codeFun]:checked").val();
+    const color = $("input:radio[name=color]:checked").val();
+    const trait = $("input:radio[name=trait]:checked").val();
+    const location = $("input:radio[name=location]:checked").val();
 
     if (!workplace || !codeFun || !color || !trait || !location) {
       alert("Please answer all of the questions to ensure a correct result.")
@@ -13,55 +13,58 @@ $(document).ready(function () {
       let ruby = 0
       let cSharp = 0
       let javaScript = 0
-      if (workplace === 1) {
+      if (workplace === "ruby") {
         ruby += 1;
-      } else if (workplace === 2) {
+      } else if (workplace === "cSharp") {
         cSharp += 1;
-      } else if (workplace === 3) {
+      } else if (workplace === "javaScript") {
         javaScript += 1;
       }
-      if (codeFun === 1) {
+      if (codeFun === "ruby") {
         ruby += 1;
-      } else if (codeFun === 2) {
+      } else if (codeFun === "cSharp") {
         cSharp += 1;
-      } else if (codeFun === 3) {
+      } else if (codeFun === "javaScript") {
         javaScript += 1;
       }
-      if (color === 1) {
+      if (color === "ruby") {
         ruby += 1;
-      } else if (color === 2) {
+      } else if (color === "cSharp") {
         cSharp += 1;
-      } else if (color === 3) {
+      } else if (color === "javaScript") {
         javaScript += 1;
       }
-      if (trait === 1) {
+      if (trait === "ruby") {
         ruby += 1;
-      } else if (trait === 2) {
+      } else if (trait === "cSharp") {
         cSharp += 1;
-      } else if (trait === 3) {
+      } else if (trait === "javaScript") {
         javaScript += 1;
       }
-      if (location === 1) {
+      if (location === "ruby") {
         ruby += 1;
-      } else if (location === 2) {
+      } else if (location === "cSharp") {
         cSharp += 1;
-      } else if (location === 3) {
+      } else if (location === "javaScript") {
         javaScript += 1;
       }
       if (ruby > cSharp && ruby > javaScript) {
         $(".languageOutput").text("Ruby");
         $("#formResult").show();
       } else if (cSharp > ruby && cSharp > javaScript) {
-        $(".languageOutput").text("JavaScript");
+        $(".languageOutput").text("C#");
         $("#formResult").show();
       } else if (javaScript > ruby && javaScript > cSharp) {
-        $(".languageOutput").text("C#");
+        $(".languageOutput").text("JavaScript");
         $("#formResult").show();
       } else {
         $(".languageOutput").text("JavaScript");
         $("#formResult").show();
       }
     }
+    $("button#reset").click(function() {
+      $("#formResult").hide();
+    })
   });
 });
 
