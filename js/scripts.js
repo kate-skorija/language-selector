a8fef1daeaf3195d33e3cd215fe4341fb2bc4a82
Kate Skorija <kateskorija@gmail.com>
2020-06-05 12:27:22 -0500

    add additional if/else statement to display results

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,64 @@ $(document).ready(function () {
     const trait = parseInt($("input:radio[name=trait]:checked").val());
     const location = parseInt($("input:radio[name=location]:checked").val());
 
+    if (!workplace || !codeFun || !color || !trait || !location) {
+      alert("Please answer all of the questions to ensure a correct result.")
+    } else {
+      let ruby = 0
+      let cSharp = 0
+      let javaScript = 0
+      if (workplace === 1) {
+        ruby += 1;
+      } else if (workplace === 2) {
+        cSharp += 1;
+      } else if (workplace === 3) {
+        javaScript += 1;
+      }
+      if (codeFun === 1) {
+        ruby += 1;
+      } else if (codeFun === 2) {
+        cSharp += 1;
+      } else if (codeFun === 3) {
+        javaScript += 1;
+      }
+      if (color === 1) {
+        ruby += 1;
+      } else if (color === 2) {
+        cSharp += 1;
+      } else if (color === 3) {
+        javaScript += 1;
+      }
+      if (trait === 1) {
+        ruby += 1;
+      } else if (trait === 2) {
+        cSharp += 1;
+      } else if (trait === 3) {
+        javaScript += 1;
+      }
+      if (location === 1) {
+        ruby += 1;
+      } else if (location === 2) {
+        cSharp += 1;
+      } else if (location === 3) {
+        javaScript += 1;
+      }
+      if (ruby > cSharp && ruby > javaScript) {
+        $(".languageOutput").text("Ruby");
+        $("#formResult").show();
+      } else if (cSharp > ruby && cSharp > javaScript) {
+        $(".languageOutput").text("JavaScript");
+        $("#formResult").show();
+      } else if (javaScript > ruby && javaScript > cSharp) {
+        $(".languageOutput").text("C#");
+        $("#formResult").show();
+      } else {
+        $(".languageOutput").text("JavaScript");
+        $("#formResult").show();
+      }
+    }
+  });
+});
+
     // if (!workplace || !codeFun || !color || !trait || !location) {
     //   alert("Please answer all of the questions to ensure a correct result.")
     // } else if (workplace === codeFun && workplace === color || workplace === codeFun && workplace === trait || workplace === codeFun && workplace === location) {
@@ -18,48 +76,3 @@ $(document).ready(function () {
     // } else if (color === trait && color === location)
     // $(".languageOutput").text(color);
     // $("#formResult").show();
-
-    if (!workplace || !codeFun || !color || !trait || !location) {
-      alert("Please answer all of the questions to ensure a correct result.")
-    } else {
-      let ruby = 0
-      let cSharp = 0
-      let javaScript = 0
-      if (workplace === 1) {
-        ruby = ruby + 1;
-      } else if (workplace === 2) {
-        cSharp = cSharp + 1;
-      } else if (workplace === 3) {
-        javaScript = javaScript + 1;
-      }
-      if (codeFun === 1) {
-        ruby = ruby + 1;
-      } else if (codeFun === 2) {
-        cSharp = cSharp + 1;
-      } else if (codeFun === 3) {
-        javaScript = javaScript + 1;
-      }
-      if (color === 1) {
-        ruby = ruby + 1;
-      } else if (color === 2) {
-        cSharp = cSharp + 1;
-      } else if (color === 3) {
-        javaScript = javaScript + 1;
-      }
-      if (trait === 1) {
-        ruby = ruby + 1;
-      } else if (trait === 2) {
-        cSharp = cSharp + 1;
-      } else if (trait === 3) {
-        javaScript = javaScript + 1;
-      }
-      if (location === 1) {
-        ruby = ruby + 1;
-      } else if (location === 2) {
-        cSharp = cSharp + 1;
-      } else if (location === 3) {
-        javaScript = javaScript + 1;
-      }
-    }
-  });
-});
